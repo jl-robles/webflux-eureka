320d243640c8c65817df7a820ed0f0619e33dbfa
jl-robles <luis.robles731@gmail.com>
2019-12-16 09:40:16 -0600

    Eureka server for spring flux services

---
+++ src/test/java/com/jlrm/springbootwebflux/eureka/SpringBootWebfluxEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlrm.springbootwebflux.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
